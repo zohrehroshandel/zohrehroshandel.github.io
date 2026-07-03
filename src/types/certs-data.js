a54ea4e7a7f3958634394922756a4558ce4695f9
zohrehroshandel <ziziroshani@gmail.com>
2026-07-04 01:12:23 +0330

    update portfolio

--- src/types/certs-data.js
+++ src/types/certs-data.js
@@ -10,7 +10,7 @@ export const certs = [
   {
     id: "kalami",
     date: "1402",
-    link: "https://faradars.org",
+    link: "https://faradars.org/verify/73AB91AD",
     icon: "📘",
     qrImage: "qr/kalami.png",
     fullImage: "certs/kalami.jpg",
@@ -18,7 +18,7 @@ export const certs = [
   {
     id: "js",
     date: "1402",
-    link: "https://faradars.org",
+    link: "https://faradars.org/verify/56018CD0",
     icon: "💻",
     qrImage: "qr/js-course.png",
     fullImage: "certs/js-course.jpg",
