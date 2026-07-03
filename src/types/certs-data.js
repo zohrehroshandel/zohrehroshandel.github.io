f32dbd97dbaccf4e2dc666858d9bd048c07699e8
zohrehroshandel <ziziroshani@gmail.com>
2026-07-04 01:30:27 +0330

    update portfolio

--- src/types/certs-data.js
+++ src/types/certs-data.js
@@ -1,29 +1,26 @@
 export const certs = [
-  {
-    id: "maktab",
-    date: "1403",
-    link: "https://maktabsharif.ir/136-Zohreh-Roshandel",
-    icon: "🎓",
-    qrImage: "qr/Maktab-sharif.png",
-    fullImage: "certs/Maktab-sharif.jpg",
-  },
-  {
-    id: "kalami",
-    date: "1402",
-    link: "https://faradars.org/verify/73AB91AD",
-    icon: "📘",
-    qrImage: "qr/kalami.png",
-    fullImage: "certs/kalami.jpg",
-  },
-  {
-    id: "js",
-    date: "1402",
-    link: "https://faradars.org/verify/56018CD0",
-    icon: "💻",
-    qrImage: "qr/js-course.png",
-    fullImage: "certs/js-course.jpg",
-  },
+	{
+		id: "maktab",
+		date: "1403",
+		link: "https://maktabsharif.ir/136-Zohreh-Roshandel",
+		icon: "🎓",
+		qrImage: "qr/Maktab-sharif.png",
+		fullImage: "certs/Maktab-sharif.jpg",
+	},
+	{
+		id: "kalami",
+		date: "1402",
+		link: "https://faradars.org/verify/73AB91AD",
+		icon: "📘",
+		qrImage: "qr/kalami.png",
+		fullImage: "certs/kalami.jpg",
+	},
+	{
+		id: "js",
+		date: "1402",
+		link: "https://faradars.org/verify/56018CD0",
+		icon: "💻",
+		qrImage: "qr/js-course.png",
+		fullImage: "certs/js-course.jpg",
+	},
 ];
-
-
-
