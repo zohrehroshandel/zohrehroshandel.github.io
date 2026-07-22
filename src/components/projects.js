daa62a2a2c598c328f8c0c3dce81e4523fcdcb7d
zohrehroshandel <ziziroshani@gmail.com>
2026-07-22 17:21:04 +0330

    style: update projects section styling

--- src/components/projects.js
+++ src/components/projects.js
@@ -30,44 +30,47 @@ function renderHanbokSection() {
 export function Projects() {
   return `
     <div class="projects-page">
-      ${renderHanbokSection()}
-      <div class="other-projects">
-        ${basicProjects
-          .map(
-            (project) => `
-              <div class="project-block glass-card">
-                <div class="project-header">
-                  <h3>${i18next.t(project.titleKey)}</h3>
-                  <p>${i18next.t(project.descKey)}</p>
-                  ${
-                    project.githubLink
-                      ? `<a href="${project.githubLink}" target="_blank" rel="noreferrer" class="github-link">GitHub Repository ↗</a>`
-                      : ""
-                  }
-                </div>
-
-                <div class="swiper projectSwiper">
-                  <div class="swiper-wrapper">
-                    ${project.images
-                      .map(
-                        (img) => `
-                          <div class="swiper-slide">
-                            <img src="${img}" class="project-img" alt="project" />
-                          </div>
-                        `
-                      )
-                      .join("")}
+      <div class="projects-container">
+        ${renderHanbokSection()}
+        <div class="other-projects">
+          ${basicProjects
+            .map(
+              (project) => `
+                <section class="project-block glass-card">
+                  <div class="project-header">
+                    <h3>${i18next.t(project.titleKey)}</h3>
+                    <p>${i18next.t(project.descKey)}</p>
+                    ${
+                      project.githubLink
+                        ? `<a href="${project.githubLink}" target="_blank" rel="noreferrer" class="github-link">GitHub Repository ↗</a>`
+                        : ""
+                    }
                   </div>
-                  <div class="swiper-pagination"></div>
-                </div>
-              </div>
-            `
-          )
-          .join("")}
+
+                  <div class="swiper projectSwiper">
+                    <div class="swiper-wrapper">
+                      ${project.images
+                        .map(
+                          (img) => `
+                            <div class="swiper-slide">
+                              <img src="${img}" class="project-img" alt="project" />
+                            </div>
+                          `
+                        )
+                        .join("")}
+                    </div>
+                    <div class="swiper-pagination"></div>
+                  </div>
+                </section>
+              `
+            )
+            .join("")}
+        </div>
       </div>
     </div>
   `;
 }
+
 
 
 
